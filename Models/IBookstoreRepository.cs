7aaf0fd89600cfc719e7c502ab5816b90379a179
Angelina Walton <waltona1@byu.edu>
2023-03-01 18:29:18 -0700

    Add project files.

---
+++ Models/IBookstoreRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission9_waltona1.Models
+{
+    //an interface is a template for a class
+    public interface IBookStoreRepository
+    {
+        IQueryable<Books> Books { get; }
+    }
+}
